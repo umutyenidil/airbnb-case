53aaea84700694a8f51e7faf49940e0049d98331
Umut Yenidil <umut.yenidil9@gmail.com>
2024-05-08 11:50:58 +0300

    init()

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+import HomePage from "./views/pages/home/home.page.jsx";
+
+const App = () => {
+    return (
+        <>
+            <HomePage/>
+        </>
+    );
+};
+
+export default App;
---
+++ src/views/pages/home/home.page.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div className='bg-red-500'>
+            Home
+        </div>
+    );
+};
+
+export default HomePage;
